063d0a041229ecc6adf0c675fad92b86b7c5d3ff
Shai Katzir <shaik@one1.co.il>
2015-07-14 13:28:45 +0300

    14-07-15

--- DoarRashum/assets/www/js/index.js
+++ DoarRashum/assets/www/js/index.js
@@ -1,14 +1,13 @@
+var appName = 'דווריים';
 document.addEventListener("deviceready", init, false);
 function init() {
 	console.log('deviceready');
 	//fastclick - https://github.com/ftlabs/fastclick	
 	FastClick.attach(document.body);
-    console.log('FastClick');
+    console.log('index.js FastClick');
     //handle client side session timeout
-    //setSessionTimeout();
-	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
-//	document.querySelector("#ManualBarCode").addEventListener("touchend", ManualBarCode, false);			
-	document.addEventListener("backbutton", onBackKeyDown, false);		
+    //setSessionTimeout();	
+	//document.addEventListener("backbutton", onBackKeyDown, false);		
 
 }
 
@@ -17,7 +16,7 @@ function startScan() {
 http://www.raymondcamden.com/2014/03/05/Barcode-Scanner-sample-and-new-repo-for-Cordova-examples
 https://github.com/wildabeast/BarcodeScanner
 */
-
+	console.log('startScan');
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {			
 			var s =
@@ -25,19 +24,32 @@ https://github.com/wildabeast/BarcodeScanner
 			"Format: " + result.format + ", " +
 			"Cancelled: " + result.cancelled;
 			console.log(s);
-			var resultDiv=document.querySelector("#scanResults");			
-			resultDiv.innerHTML = result.text;					
-			resultDiv.style.display="block";
-			//enable takepic1 button
-			if (!(result.cancelled)) {
-				$("#takePic1").removeAttr('disabled');
-			}
-			//enable cancel button
-			$("#Cancel").removeAttr('disabled');
+			//var resultDiv=document.getElementById(divResult);			
+			//resultDiv.innerHTML = result.text;					
+			//resultDiv.style.display="block";
 		}, 
 		function (error) {
 			alert("Scanning failed: " + error);
 		}
 	);
 
+}
+
+//handle back button
+function onBackKeyDown() {
+	navigator.notification.confirm(
+									'האם אתה בטוח שברצונך לצאת מהיישום?', 		  //message
+									exitFromApp, 		              //callback function
+									appName,                        //title
+									['יציאה', 'ביטול']);                  //button labels
+}									
+
+function exitFromApp(buttonIndex) {
+        if (buttonIndex === 1) {
+			if (navigator.app) {
+				navigator.app.exitApp();
+			} else if (navigator.device) {
+				navigator.device.exitApp();
+              }
+		}
 }
